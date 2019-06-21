69741595ce5d3861e26608b378d21cfb71af97f8
kristen <kristenmay0520@gmail.com>
2019-06-21 12:29:07 -0400

    first commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,32 @@
+package com.example;
+
+public class Main {
+
+    public static int COUNTER = 0;
+
+    public static void main(String[] args) {
+        int counter = 0;
+        for(int i=999; i<1051; i++){
+            isPrime(i);
+            if(COUNTER == 3){
+                break;
+            }
+        }
+    }
+
+    public static boolean isPrime(int n){
+
+        if(n == 1){
+            return false;
+        }
+
+        for(int i=2; i <= n/2; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        COUNTER++;
+        System.out.println(n + " is a prime number");
+        return true;
+    }
+}
